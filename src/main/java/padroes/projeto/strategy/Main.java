8da05c7b3cfd6ccc49436911c255f68bb0c7b15c
Emanuel_R <emanuelreino13@gmail.com>
2024-07-08 13:26:57 -0300

    Criado as classes envio economico e envio padrao, onde o peso é multiplicado pelo valor indicado. No main foi aplicado o método strategy e atribuido a variavel do custo correspondente.

--- src/main/java/padroes/projeto/strategy/Main.java
+++ src/main/java/padroes/projeto/strategy/Main.java
@@ -1,17 +1,19 @@
 package padroes.projeto.strategy;
 
 public class Main {
-
     public static void main(String[] args) {
         CalculadoraCustoEnvio calculadora = new CalculadoraCustoEnvio();
 
-        // Configura a estratégia de cálculo de custo de envio como "envio expresso"
         calculadora.setStrategy(new EnvioExpresso());
         double custoEnvioExpresso = calculadora.calcularCustoEnvio(5.0);
         System.out.println("Custo de envio expresso: $" + custoEnvioExpresso);
 
-        // Configure a estratégia de cálculo de custo de envio como "envio padrao"
+        calculadora.setStrategy(new EnvioPadrao());
+        double custoEnvioPadrao = calculadora.calcularCustoEnvio(5.0);
+        System.out.println("Custo de envio padrão: $" + custoEnvioPadrao);
 
-        // Configure a estratégia de cálculo de custo de envio como "envio economico"
+        calculadora.setStrategy(new EnvioEconomico());
+        double custoEnvioEconomico = calculadora.calcularCustoEnvio(5.0);
+        System.out.println("Custo de envio econômico: $" + custoEnvioEconomico);
     }
 }
